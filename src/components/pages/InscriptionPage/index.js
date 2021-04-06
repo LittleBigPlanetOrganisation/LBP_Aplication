3771bddc5e30f51ae826fbfebe986ecd06dbf9c3
Krystel <Krystelmenot@gmail.com>
2021-04-07 00:29:27 +0200

    Begin inscriptionPage

---
+++ src/components/pages/InscriptionPage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+import Page from "../../../Page";
+
+export default function InscriptionPage() {
+  return (
+    <Page>
+      <section className="des">
+        <Typography variant="h5">
+          'Organisez, partagez et imprimez vos voyages en toute simplicité'
+        </Typography>
+      </section>
+    </Page>
+  );
+}
